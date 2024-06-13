7d2faf449acb0d623bdebbb3c06d9331dc731014
Ikevine <ishimwekevine098@gmail.com>
2024-06-13 07:43:33 +0200

    Auth login and register complete

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import{usersRoutes}from './routes/userRouters.js'
 dotenv.config();
+
+
 //creating app
 const app = express();
+app.use(express.json());
+
+//importing route
+app.use('/api/users', usersRoutes)
+
 
 //Connecting to db and listen to port
 mongoose.connect(process.env.MONGO)
@@ -16,3 +24,5 @@ mongoose.connect(process.env.MONGO)
 })
 
 .catch((error)=> console.log("Error during connection:: ",error))
+
+
